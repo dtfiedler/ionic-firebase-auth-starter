11078a1dc30b6ab24c189ef058c2ca66349b2cdb
Dylan Fiedler <dylan.fiedler@ge.com>
2018-05-14 17:32:24 -0500

    adding firebase key template

---
+++ firebase.js
@@ -0,0 +1,10 @@
+module.exports = {
+    firebaseConfigs: {
+        apiKey: "INSERT KEY HERE",
+        authDomain: "INSERT KEY HERE",
+        databaseURL: "INSERT KEY HERE",
+        projectId: "INSERT KEY HERE",
+        storageBucket: "INSERT KEY HERE",
+        messagingSenderId: "INSERT KEY HERE"
+    }
+}
